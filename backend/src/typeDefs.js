54310886a5215d5b4221cd87fbe931be84954fdb
Gabriel Custódio <gbrlcustodio@outlook.com>
2019-04-06 14:43:09 -0300

    creates custom graphql query

--- backend/src/typeDefs.js
+++ backend/src/typeDefs.js
@@ -1,9 +1,13 @@
 const typeDefs = `
 type BlogPost {
+  id: ID
   content: String
   createdAt: DateTime
   title: String
   updatedAt: DateTime
+}
+type Query {
+  AllBlogPosts: [BlogPost]
 }
 `;
 
